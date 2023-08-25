eca847647d7d466c8a4727499faa17c2b10c88a5
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2023-08-24 18:55:28 -0700

    edit images

--- src/modules/views/BackgroundImage.js
+++ src/modules/views/BackgroundImage.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import DriftImg from '../../assests/DriftImg.jpeg'
 
 
 function BackgroundImage(){
     return (
         <div className='background-image'>
-            {/* <img src={DriftImg} alt='Drift-Mobile' /> */}
         </div>
     )
 }
